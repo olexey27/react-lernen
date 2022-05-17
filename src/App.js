a38afda5a73a45aed603817dd2087698af01f886
Alexey <allxyog@gmail.com>
2022-05-17 15:01:58 +0200

    Extra Aufgabe

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ function App() {
   return (
     <div className="App">
       <h1>Hallo Welt!</h1>
-        <h2>Ganz schön drekig hier!</h2>
+        <br/>
+        <h2>HTML/CSS/JS!</h2>
+        <h3>
+            <i>
+                <b>Kenntnisse</b>
+            </i>
+        </h3>
+        <progress min = "0" max = "100" value = "79"></progress>
+        <label for = "file">HTML </label>
+        <br/>
+        <br/>
+        <progress min = "0" max = "100" value = "70"></progress>
+        <label for = "file">CSS</label>
+        <br/>
+        <br/>
+        <progress min = "0" max = "100" value = "62"></progress>
+        <label for = "file">CJ</label>
     </div>
   );
 }
